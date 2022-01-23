fb7730bf1ae478b625c0dc026c5ba15a3911c47d
Hüseyin Küçükşahin <hksahin06@gmail.com>
2022-01-23 14:58:06 +0300

    uhu

--- TestCam/Form1.cs
+++ TestCam/Form1.cs
@@ -17,7 +17,7 @@ namespace TestCam
         private double Xcm, Ycm, Zcm = 50.0;
         public double d1 = 3.5;
 
-        //Ercan 65
+        //Ercan 651231
 
         static SerialPort _serialPort;
         public byte[] Buff = new byte[2];
